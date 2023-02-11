8282017682b5ec477aa82138c7ba4f8b9f142980
Callum Freeman-Davis <Callum Freeman-Davis>
2023-02-11 22:28:32 +0000

    gait updates, streamlined some stuff

--- Hexapod_v0.2/Hexapod_v0.2.ino
+++ Hexapod_v0.2/Hexapod_v0.2.ino
@@ -1,8 +1,9 @@
  #include <math.h>
 #include <Arduino.h>
-#include "ServoEasing.hpp"
-#define DISABLE_COMPLEX_FUNCTIONS 
 #define ENABLE_EASE_CUBIC
+#define ENABLE_EASE_QUADRATIC
+#include "ServoEasing.hpp"
+
 
 //# of Legs
 int numLegs = 6;
@@ -115,7 +116,12 @@ void setup() {
 
 
 void loop() {
-    triWalk(40,1,0,70);
+  /*for (int i = 0; i<5; i++){
+    triWalk(40,1,0,100);
+  }*/
+  for (int i = 0; i<5; i++){
+    biRipple(40,1,0,100);
+  }
 }
 
 
@@ -123,7 +129,7 @@ void loop() {
 void inverseKinematics(Leg legNumber, float x, float y, float z, float velocity, bool interrupt) {
   float q1 = atan2(y, x);
   float coxa = l1 * cos(q1);  // adjusted length of coxa
-  float d = sqrt((x * x) + (y * y)) - l1;
+  float d = sqrt((x * x) + (y * y)) - coxa;
   float lc = sqrt((d * d) + ((z - l1) * (z - l1)));
   float alpha = acos(((l2 * l2) + (lc * lc) - (l3 * l3)) / (2 * l2 * lc));
   float beta = atan2(z - l1, d);
@@ -182,14 +188,15 @@ if (q3 <= 0)
   q3 = map(q3, -180, -140, 40,0);
 }
 
-if (interrupt == true) {
+  moveLeg(legNumber, q1, q2, q3, velocity, interrupt);
+/*if (interrupt == true) {
 moveLeg2(legNumber, q1, q2, q3, velocity);
 }
 else {
   moveLeg(legNumber, q1, q2, q3, velocity);
+}*/
 }
-}
-
+/*
 void moveLeg2(Leg legNumber, float q1, float q2, float q3, float velocity) { //tri only, testing interrupts
   float q1l = map(q1, 0, 180, 180, 0);
   float q2l = map(q2, 0, 180, 180, 0);
@@ -330,8 +337,8 @@ void moveLeg2(Leg legNumber, float q1, float q2, float q3, float velocity) { //t
      default:
      return;
   }
-}
-void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
+}*/
+void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity, bool interrupt) {
   //q1 = map(q1, 0, 180, 0, 180);
   //q2 = map(q2, 0, 180, 0, 180);
   //q3 = map(q3, 0, 180, 0, 180);
@@ -345,7 +352,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legFL[0].setEaseTo(q1l);
       legFL[1].setEaseTo(q2l);
       legFL[2].setEaseTo(q3l);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
     case 1: 
@@ -353,7 +361,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legFR[0].setEaseTo(q1);
       legFR[1].setEaseTo(q2);
       legFR[2].setEaseTo(q3);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 2: 
@@ -361,7 +370,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legML[0].setEaseTo(q1l);
       legML[1].setEaseTo(q2l);
       legML[2].setEaseTo(q3l);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 3:  
@@ -369,7 +379,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legMR[0].setEaseTo(q1);
       legMR[1].setEaseTo(q2);
       legMR[2].setEaseTo(q3);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 4:  
@@ -377,7 +388,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legRL[0].setEaseTo(q1l);
       legRL[1].setEaseTo(q2l);
       legRL[2].setEaseTo(q3l);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 5:  
@@ -385,7 +397,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legRR[0].setEaseTo(q1);
       legRR[1].setEaseTo(q2);
       legRR[2].setEaseTo(q3);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 6: //all  
@@ -408,7 +421,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legRR[0].setEaseTo(q1);
       legRR[1].setEaseTo(q2);
       legRR[2].setEaseTo(q3);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 7:  //pair1
@@ -419,7 +433,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legRR[0].setEaseTo(q1);
       legRR[1].setEaseTo(q2);
       legRR[2].setEaseTo(q3);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 8:  //pair2
@@ -430,7 +445,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legML[0].setEaseTo(q1l);
       legML[1].setEaseTo(q2l);
       legML[2].setEaseTo(q3l);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 9:  //pair3
@@ -441,7 +457,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legRL[0].setEaseTo(q1l);
       legRL[1].setEaseTo(q2l);
       legRL[2].setEaseTo(q3l);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 10:  //tri1
@@ -455,7 +472,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legRL[0].setEaseTo(q1l);
       legRL[1].setEaseTo(q2l);
       legRL[2].setEaseTo(q3l);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt == true) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      case 11:  //tri2
@@ -469,7 +487,8 @@ void moveLeg(Leg legNumber, float q1, float q2, float q3, float velocity) {
       legRR[0].setEaseTo(q1);
       legRR[1].setEaseTo(q2);
       legRR[2].setEaseTo(q3);
-      synchronizeAllServosStartAndWaitForAllServosToStop();
+      if (interrupt == true) setEaseToForAllServosSynchronizeAndStartInterrupt(30);
+      else synchronizeAllServosStartAndWaitForAllServosToStop();
      break;
 
      default:
@@ -496,16 +515,40 @@ void triWalk(float strideLength, float strideWidth, float strideLift, float velo
   //Step 1;
   inverseKinematics(tri1, mid, strideWidth, strideLift + 45, velocity, true);   //lift tri 1
   inverseKinematics(tri2, back, strideWidth, strideLift, velocity, false); //move tri 2 - body moving
-  inverseKinematics(tri1, ward, strideWidth, strideLift, velocity, false);   //drop tri 1
+  inverseKinematics(tri1, ward, strideWidth, strideLift, velocity*1.5, false);   //drop tri 1
   
 
   //Step 2;
   inverseKinematics(tri2, mid, strideWidth, strideLift + 45, velocity, true); //lift tri 2
   inverseKinematics(tri1, back, strideWidth, strideLift, velocity, false); //move tri 1 - body moving
-  inverseKinematics(tri2, ward, strideWidth, strideLift, velocity, false); //drop tri 2
+  inverseKinematics(tri2, ward, strideWidth, strideLift, velocity*1.5, false); //drop tri 2
 }
 
-/*
+void biRipple(float strideLength, float strideWidth, float strideLift, float velocity) {
+  strideLift = -95 - strideLift;
+  strideWidth = 75 + strideWidth;
+  float returnVelocity = velocity/3;
+  float back = (strideLength/2) - strideLength;
+  float mid = (strideLength/2) - (strideLength/2) + 5;
+  float ward = strideLength/2;
+
+  inverseKinematics(pair1, mid, strideWidth, strideLift + 45, velocity, true);   //lift pair 1
+  inverseKinematics(pair2, back, strideWidth, strideLift, returnVelocity, true); //move pair 2 - body moving
+  inverseKinematics(pair3, mid, strideWidth, strideLift, returnVelocity, false); //move pair 3 - body moving
+  inverseKinematics(pair1, ward, strideWidth, strideLift, velocity, false);   //drop pair 1
+  
+  inverseKinematics(pair2, mid, strideWidth, strideLift + 45, velocity, true);   //lift pair 2
+  inverseKinematics(pair3, back, strideWidth, strideLift, returnVelocity, true); //move pair 3 - body moving
+  inverseKinematics(pair1, mid, strideWidth, strideLift, returnVelocity, false); //move pair 1 - body moving
+  inverseKinematics(pair2, ward, strideWidth, strideLift, velocity, false);   //drop pair 2
+  
+  inverseKinematics(pair3, mid, strideWidth, strideLift + 45, velocity, true);   //lift pair 3
+  inverseKinematics(pair1, back, strideWidth, strideLift, returnVelocity, true); //move pair 1 - body moving
+  inverseKinematics(pair2, mid, strideWidth, strideLift, returnVelocity, false); //move pair 2 - body moving
+  inverseKinematics(pair3, ward, strideWidth, strideLift, velocity, false);   //drop pair 3
+}
+
+
 void forwardKinematics(float q1, float q2, float q3) {
   q1 = q1 * PI / 180;  // converting to radians
   q2 = q2 * PI / 180;
@@ -522,7 +565,5 @@ void forwardKinematics(float q1, float q2, float q3) {
   Serial.print(y);
   Serial.print(" z: ");
   Serial.println(z);
-  inverseKinematics(x,y,z);
   
 }
-*/
